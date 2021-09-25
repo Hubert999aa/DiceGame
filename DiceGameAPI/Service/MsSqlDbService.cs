180dc03b5f11d912c6cdad12cdbcf898d0f9e33c
Hubert <golab.mlody@gmail.com>
2021-09-25 14:31:45 +0200

    Add MsSql service methods implementation

--- DiceGameAPI/Service/MsSqlDbService.cs
+++ DiceGameAPI/Service/MsSqlDbService.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Linq;
 using DiceGameAPI.Models;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using DiceGameAPI.Enumerators;
 
 namespace DiceGameAPI.Service
 {
@@ -59,56 +60,187 @@ namespace DiceGameAPI.Service
             _context.SaveChanges();
         }
 
-        public int ChangePlayerName(int playerId)
+        public int ChangePlayerName(int playerId, string newName)
         {
-            throw new NotImplementedException();
+            var player = _context.Players
+                                 .Where(p => p.IdPlayer == playerId)
+                                 .Single();
+
+            player.Name = newName;
+
+            _context.SaveChanges();
+
+            return player.IdPlayer;
         }
 
         public Player GetPlayer(int playerId)
         {
-            throw new NotImplementedException();
+            var player = _context.Players
+                                 .Where(p => p.IdPlayer == playerId)
+                                 .Single();
+
+            return player;
         }
         public Game GetGame(int gameId)
         {
-            throw new NotImplementedException();
+            var game = _context.Games
+                               .Where(p => p.IdGame == gameId)
+                               .Single();
+
+            return game;
         }
         public PointsTable GetPointsTable(int pointsTableId)
         {
-            throw new NotImplementedException();
+            var pointsTable = _context.PointsTables
+                                      .Where(p => p.IdPointsTable == pointsTableId)
+                                      .Single();
+
+            return pointsTable;
         }
         public PlayerGameHistory GetPlayerGameHistory(int playerId, int gameId)
         {
-            throw new NotImplementedException();
+            var history = _context.PlayerGameHistories
+                                  .Where(p => p.IdGame == gameId)
+                                  .Where(p => p.IdPlayer == playerId)
+                                  .Single();
+
+            return history;
         }
 
         public void AddPointsToTable(int pointsTableId, int points, string field)
         {
-            throw new NotImplementedException();
+            var pointsTable = _context.PointsTables
+                                      .Where(p => p.IdPointsTable == pointsTableId)
+                                      .Single();
+
+            var properties = pointsTable.GetType().GetProperties().ToList();
+            foreach(var prop in properties)
+            {
+                if (prop.Name.Equals(field))
+                {
+                    prop.SetValue(pointsTable, points);
+                    break;
+                }
+            }
+
+            _context.SaveChanges();
         }
         public void ChangePoints(int pointsTableId, int oldValue, int newValue, string field)
         {
-            throw new NotImplementedException();
+            AddPointsToTable(pointsTableId, newValue, field);
+            var pointsTable = _context.PointsTables
+                                      .Where(p => p.IdPointsTable == pointsTableId)
+                                      .Single();
+
+            var properties = pointsTable.GetType().GetProperties().ToList();
+            foreach (var prop in properties)
+            {
+                if (prop.Name.Equals(field))
+                {
+                    if ((int)prop.GetValue(pointsTable) == oldValue)
+                    {
+                        throw new Exception("Change value failied");
+                    }
+                    break;
+                }
+            }
         }
         public int GetAllPoints(int pointsTableId)
         {
-            throw new NotImplementedException();
+            var pointsTable = _context.PointsTables
+                                      .Where(p => p.IdPointsTable == pointsTableId)
+                                      .Single();
+
+            if (!CheckIfSchoolFieldsAreFilled(pointsTable) || !CheckIfSecondPartOfPointsFieldsAreFilled(pointsTable))
+            {
+                throw new Exception("To sum up all points, first you need to fill all fields with points.");
+            }
+
+            var schoolPoints = GetSummaryPointsFromSchool(pointsTableId);
+            var secondPartOfPoints = pointsTable.Pair + pointsTable.TwoPairs + pointsTable.ThreeOfKind + pointsTable.FourOfKind
+                + pointsTable.FullHouse + pointsTable.General + pointsTable.Chance + pointsTable.SmallStraight + pointsTable.LargeStraight;
+
+            return schoolPoints + (int)secondPartOfPoints;
         }
         public int GetSummaryPointsFromSchool(int pointsTableId)
         {
-            throw new NotImplementedException();
+            var pointsTable = _context.PointsTables.Where(p => p.IdPointsTable == pointsTableId).Single();
+
+            if (CheckIfSchoolFieldsAreFilled(pointsTable))
+            {
+                throw new Exception("To sum up school points first you need to fill all school fields");
+            }
+
+            var schoolPoints = pointsTable.Aces + pointsTable.Twos + pointsTable.Threes + pointsTable.Fours + pointsTable.Fives + pointsTable.Sixes;
+            if (schoolPoints >= 0)
+            {
+                return (int)(50 + schoolPoints);
+            }
+            else
+            {
+                return (int)(0 - schoolPoints - 50);
+            }
         }
 
-        public void FinishGame(int gameId)
+        public void FinishGame(int gameId, int firstPlayerId, int secondPlayerId)
         {
-            throw new NotImplementedException();
+            var firstPlayerGameHistory = _context.PlayerGameHistories
+                                                 .Include(p => p.PointsTable)
+                                                 .Include(p => p.Game)
+                                                 .Where(p => p.IdGame == gameId)
+                                                 .Where(p => p.IdPlayer == firstPlayerId)
+                                                 .Single();
+
+            var secondPlayerGameHistory = _context.PlayerGameHistories
+                                                  .Include(p => p.PointsTable)
+                                                  .Include(p => p.Game)
+                                                  .Where(p => p.IdGame == gameId)
+                                                  .Where(p => p.IdPlayer == secondPlayerId)
+                                                  .Single();
+
+            var firstPlayerPoints = GetAllPoints(firstPlayerGameHistory.IdPointsTable);
+            var secondPlayerPoints = GetAllPoints(firstPlayerGameHistory.IdPointsTable);
+
+            if (firstPlayerPoints == secondPlayerPoints)
+            {
+                firstPlayerGameHistory.GameResult = GameResult.Draw;
+                secondPlayerGameHistory.GameResult = GameResult.Draw;
+            }
+            else
+            {
+                if (firstPlayerPoints > secondPlayerPoints)
+                {
+                    firstPlayerGameHistory.GameResult = GameResult.Won;
+                    secondPlayerGameHistory.GameResult = GameResult.Lost;
+                }
+                else
+                {
+                    firstPlayerGameHistory.GameResult = GameResult.Lost;
+                    secondPlayerGameHistory.GameResult = GameResult.Won;
+                }
+            }
+
+            firstPlayerGameHistory.Game.Finished = true;
+
+            _context.SaveChanges();
         }
-        public void SetLooser(int playerId)
+
+        private bool CheckIfSchoolFieldsAreFilled(PointsTable pointsTable)
         {
-            throw new NotImplementedException();
+            if (pointsTable.Aces == null || pointsTable.Twos == null || pointsTable.Threes == null || pointsTable.Fours == null || pointsTable.Fives == null || pointsTable.Sixes == null)
+            {
+                return false;
+            }
+            return true;
         }
-        public void SetWinner(int playerId)
+        private bool CheckIfSecondPartOfPointsFieldsAreFilled(PointsTable pointsTable)
         {
-            throw new NotImplementedException();
+            if (pointsTable.Pair == null || pointsTable.TwoPairs == null || pointsTable.ThreeOfKind == null || pointsTable.FourOfKind == null || pointsTable.FullHouse == null
+                || pointsTable.SmallStraight == null || pointsTable.LargeStraight == null || pointsTable.General == null || pointsTable.Chance == null)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
